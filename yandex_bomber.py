7e9e37326ba9a5b384864c75f5c5bfcc6019a985
Artur-Babayan <56497130+Artur-Babayan@users.noreply.github.com>
2024-05-26 12:49:40 +0400

    Update yandex_bomber.py

--- yandex_bomber.py
+++ yandex_bomber.py
@@ -1,45 +1,3 @@
-### Old version ######
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from webdriver_manager.chrome import ChromeDriverManager
-# from fake_useragent import UserAgent
-# import time
-#
-# inp_number = input("Please input number: ")
-# phone_number = "+374" + inp_number
-#
-# ua = UserAgent()
-# user_agent = ua.random
-#
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument(f'user-agent={user_agent}')
-#
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#
-# try:
-#     driver.get('https://move2armenia.am/ru/aviacompanies/utair/')
-#     login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.user-area.signin-area a[href='#register']")))
-#     login_button.click()
-#     phone_input = WebDriverWait(driver, 5).until(
-#         EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='tel'][name='phone']"))
-#     )
-#     phone_input.clear()
-#     phone_input.send_keys(phone_number)
-#
-#     get_code_button = WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable((By.CSS_SELECTOR, "form#get-code .form__submit .btn"))
-#     )
-#     get_code_button.click()
-#
-#     time.sleep(5)
-# finally:
-#     driver.quit()
-
-
 ######## New version #######
 
 import logging
@@ -53,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from fake_useragent import UserAgent
 
 # Настройка логирования
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='script.log')
 logger = logging.getLogger(__name__)
 
 class Move2ArmeniaLogin:
@@ -113,7 +71,7 @@ class Move2ArmeniaLogin:
 
 if __name__ == "__main__":
     # phone_number = input("Please input number: ")
-    numbers = ['94328278', '95555229']
+    numbers = ['12345678', '12345678']
     for phone_number in numbers:
         url = 'https://move2armenia.am/ru/aviacompanies/utair/'
         m2a_login = Move2ArmeniaLogin(phone_number)

deb13e10cb4ffa951543fe6ffe37bc1c75af8f77
Artur-Babayan <56497130+Artur-Babayan@users.noreply.github.com>
2024-05-26 12:46:57 +0400

    Update sms_bomber.py

--- sms_bomber.py
+++ sms_bomber.py
@@ -1,38 +1,3 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from webdriver_manager.chrome import ChromeDriverManager
-# from fake_useragent import UserAgent
-# import time
-#
-# # inp_number = input("Please input number: ")
-# # phone_number = "+374" + inp_number
-# numbers = ['95087868', '94328278', '95555229']
-# code = '+374'
-# for number in numbers:
-#     phone_number = code + number
-#     ua = UserAgent()
-#     user_agent = ua.random
-#
-#     chrome_options = webdriver.ChromeOptions()
-#     chrome_options.add_argument(f'user-agent={user_agent}')
-#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#     try:
-#         driver.get('https://passport.yandex.ru/auth/reg/portal')
-#         phone_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "passp-field-phone")))
-#         phone_input.clear()
-#         phone_input.send_keys(phone_number)
-#         next_button = driver.find_element(By.ID, "passp:phone:controls:next")
-#         next_button.click()
-#         time.sleep(5)
-#         print(f"SMS sended to number {phone_number}")
-#     finally:
-#         driver.quit()
-
-
-
 import logging
 import time
 from selenium import webdriver
@@ -101,7 +66,7 @@ class YandexSMSLogin:
                 self.close_driver()
 
 if __name__ == "__main__":
-    numbers = ['95087868', '94328278', '95555229']
+    numbers = ['12345678', '12345678', '12345678']
     code = '+374'
     url = 'https://passport.yandex.ru/auth/reg/portal'
     yandex_sms_login = YandexSMSLogin(numbers, code)
